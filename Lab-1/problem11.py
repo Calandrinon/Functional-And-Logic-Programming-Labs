a5850a0fffbdbab897e04936afb251f5963d65ce
Calandrinon <georgesut@yahoo.com>
2020-10-06 15:49:25 +0300

    Add mathematical model and implementation

---
+++ Lab-1/problem11.py
@@ -0,0 +1,104 @@
+class Node:
+    def __init__(self, element):
+        self.element = element
+        self.next = None
+    
+class MyList:
+    def __init__(self):
+        self.head = None
+
+
+def create_list():
+    my_list = MyList()
+    my_list.head = create_list_recursively()
+    return my_list 
+
+
+def create_list_recursively():
+    x = int(input("x="))
+    if x == 0:
+        return None
+    else:
+        node = Node(x)
+        node.next = create_list_recursively()
+        return node
+
+
+def is_empty(my_list):
+    if my_list.head == None:
+        return True
+    return False
+
+    
+def print_list(list):
+    print_list_recursively(list.head)
+
+    
+def print_list_recursively(node):
+    if node != None:
+        print(node.element)
+        print_list_recursively(node.next)
+        
+"""
+11.
+a. Determine if a certain element is member in a list.
+b. Determine the length of a list.
+"""
+
+        
+        
+def is_member(my_list, element):
+    if is_empty(my_list):
+        return False
+    
+    if my_list.head.element == element:
+        return True 
+
+    new_list_parameter = MyList() 
+    new_list_parameter.head = my_list.head.next 
+    return is_member(new_list_parameter, element)
+
+
+def list_length(my_list):
+    if is_empty(my_list):
+        return 0
+
+    new_list_parameter = MyList() 
+    new_list_parameter.head = my_list.head.next 
+    return 1 + list_length(new_list_parameter)
+
+
+def test_member():
+    test_list = [2, 6, 4, 2, 1, 9]
+    assert(is_member(test_list, 1) == True)
+    assert(is_member(test_list, 222) == False)
+    test_list = []
+    assert(is_member(test_list, 1) == False)
+    print("Is_member test passed.")
+
+
+def test_length():
+    test_list = [2, 6, 4, 2, 1, 9]
+    assert(list_length(test_list) == 6)
+    test_list = []
+    assert(list_length(test_list) == 0)
+    test_list = [1, 2, 5]
+    assert(list_length(test_list) == 3)
+    print("List_length test passed.")
+
+
+def test():
+    list = create_list()
+    print_list(list)
+
+    member = is_member(list, 5)
+    if member == True:
+        print("5 is a member of the list.")
+    else:
+        print("5 is not a member of the list.")
+
+    length = list_length(list)
+    print("The length of the list is {}.".format(length))
+
+test()
+
