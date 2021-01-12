dce82ef71d1afa4dca5c28a5708bb640685c176c
Calandrinon <georgesut@yahoo.com>
2021-01-12 07:14:11 +0200

    Replace ambiguous parameter name "list" to "x"

--- Lab-7/problem5.lisp
+++ Lab-7/problem5.lisp
@@ -5,15 +5,15 @@
 
 
 #|
-	sums(list) =
-		list, list is an even number
-		-list, list is an odd number
-		Σ i=(1,n) sums(list_i), otherwise (in this case, 'list' is actually a list)	
+	sums(x) =
+		x, x is an even number
+		-x, x is an odd number
+		sigma i=(1,n) sums(x_i), otherwise (x is a list)	
 |#
 
-(defun sums (list)
+(defun sums (x)
 	(cond
-	  	((and (numberp list) (evenp list)) list)
-	  	((and (numberp list) (oddp list)) (- 0 list))
-		(t (apply #'+ (mapcar #'sums list)))	
+	  	((and (numberp x) (evenp x)) x)
+	  	((and (numberp x) (oddp x)) (- 0 x))
+		(t (apply #'+ (mapcar #'sums x)))	
 	))
